aed1c6dd637312e23038e6a880c1647819301f6d
afrizaldwi <afrizaldwisaga@gmail.com>
2023-10-03 16:20:00 +0700

    change contact

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,26 @@
 const navbar = document.querySelector("nav");
 
 window.onscroll = () => {
-    if (this.scrollY > 10) {
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
-    }
+  if (this.scrollY > 10) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
 };
 
 // back to top
 const backToTopBtn = document.getElementById("backToTopBtn");
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 100) {
-        backToTopBtn.classList.add("show");
-    } else {
-        backToTopBtn.classList.remove("show");
-    }
+  if (window.scrollY > 100) {
+    backToTopBtn.classList.add("show");
+  } else {
+    backToTopBtn.classList.remove("show");
+  }
 });
 
 backToTopBtn.addEventListener("click", () => {
-    window.scrollTo({ top: 0 });
+  window.scrollTo({ top: 0 });
 });
 
 // certificate
@@ -31,14 +31,14 @@ const modalClose = document.getElementById("modalClose");
 const certificatePreview = document.getElementById("certificatePreview");
 
 certificateImg.forEach((image) => {
-    image.addEventListener("click", () => {
-        modalSm.classList.toggle("show");
-        certificatePreview.src = image.src;
-        document.body.style.overflow = "hidden";
-    });
+  image.addEventListener("click", () => {
+    modalSm.classList.toggle("show");
+    certificatePreview.src = image.src;
+    document.body.style.overflow = "hidden";
+  });
 });
 
 modalClose.addEventListener("click", () => {
-    modalSm.classList.toggle("show");
-    document.body.style.overflow = "auto";
+  modalSm.classList.toggle("show");
+  document.body.style.overflow = "auto";
 });
